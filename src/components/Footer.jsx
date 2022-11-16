f915f068a3f488f4e16e0def9abb4714005ec9cb
dasimems <isaacseun63@gmail.com>
2022-11-17 00:00:05 +0100

    feat: added the footer section without doing the responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,163 @@
-import React from "react";
+import { Button, Col, Row } from "antd";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import logo from "../asset/images/logo.png";
-import { socials, links, footerContact } from "../utils/constant";
+import {FaArrowRight} from "react-icons/fa"
+import { socials, links, service, footerContact } from "../utils/constant";
 
 function Footer(props) {
+
+    const [newsletterContent, setNewsletterContent] = useState("");
+
   return (
-    <></>
+    <>
+    
+      <footer>
+
+        <Row justify="center" className="footer-container">
+
+          <Col span={22}>
+
+            <Row justify="space-between" className="footer-logo-content">
+
+              <div className="footer-logo">
+
+                <img src={logo} alt="crispy logo" />
+
+              </div>
+
+            </Row>  
+
+            <Row justify="space-between" className="footer-content">
+
+              <Col span={6} className="footer-contact">
+
+                <h1>Subscribe to Our Newsletter</h1>
+
+                <div className="form-content">
+
+                  <input value={newsletterContent} onChange={(e)=>{
+                    setNewsletterContent(e.target.value);
+                  }} className="newsletter-input" id="newsletter-input" placeholder="Please enter your email" />
+                  <Button className="subscribe-button"><FaArrowRight /></Button>
+                </div>
+
+                <ul className="social-links">
+
+                  {socials.map((social, index)=>{
+
+                    var {icon, url} = social;
+                    return(
+                      <>
+
+                        <li key={index}><a href={url} target="_blank" rel="noreferrer" >{icon}</a></li>
+
+                      </>
+                    )
+                  })}
+
+                </ul>
+
+
+              </Col>
+
+              <Col span={4} className="footer-service">
+
+                  <h2 className="footer-title">Our Services</h2>
+
+                  <ul className="link">
+
+                    {service.map((service, index)=>{
+                      var { title} = service
+                      return(
+                        <>
+                        
+                          <li key={index}>
+
+                            {title}
+
+                          </li>
+
+                        </>
+                      )
+                    })}
+
+                  </ul>
+
+              </Col>
+
+              <Col span={4} className="footer-links">
+
+                  <h2 className="footer-title">Quick Links</h2>
+
+                  <ul className="link">
+
+                    {links.map((contacts, index)=>{
+                      var { url, text} = contacts
+                      return(
+                        <>
+                        
+                          <li key={index}>
+
+                            <Link to={url}>
+
+                              {/* <span className="icon">{icon}</span> */}
+                              <span className="text">{text}</span>
+                              
+                            </Link>
+
+                          </li>
+
+                        </>
+                      )
+                    })}
+
+                  </ul>
+
+              </Col>
+
+              <Col span={6} className="footer-about">
+
+                  <h2 className="footer-title">About Us</h2>
+
+                  <ul className="link">
+
+                    {footerContact.map((contacts, index)=>{
+                      var {icon, url, text} = contacts
+                      return(
+                        <>
+                        
+                          <li key={index}>
+
+                            <a href={url} target="_blank" rel="noreferrer">
+
+                              <span className="icon">{icon}</span>
+                              <span className="text">{text}</span>
+                              
+                            </a>
+
+                          </li>
+
+                        </>
+                      )
+                    })}
+
+                  </ul>
+
+              </Col>
+
+            </Row>
+
+
+
+          </Col>
+
+        </Row>
+
+      </footer>
+
+    </>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import { FaBars } from "react-icons/fa";
+// import { FaBars } from "react-icons/fa";
 
 import { links } from "../utils/constant";
 
